7bc76a4dc284ac505b81861f811588d10a0ce5e1
brpgrrtygng <74527999+svehlap7@users.noreply.github.com>
2023-03-28 12:03:21 +0200

    Add files via upload

---
+++ hernipostava.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OPP_TEST
+{
+    internal class hernipostava
+    {
+        private string jmeno;
+        private string prijmeni;
+        private int pozx = 0;
+        private int pozy = 0;
+        public string Jmeno
+        {
+            get
+            {
+                return jmeno;
+            }
+            private set
+            {
+                jmeno = Jmeno.ToString();
+            }
+        }
+         public int level = 1;
+
+        public double Pozx
+        {
+            get
+            {
+                return pozx;
+            }
+            private set
+            {
+            }
+        }
+
+        public double Pozy
+        {
+            get
+            {
+                return pozy;
+            }
+            private set
+            {
+            }
+        }
+
+        public hernipostava(string jmeno, string prijmeni)
+        {
+            this.Jmeno = jmeno;
+            this.prijmeni = prijmeni;
+        }
+
+        //public int Zmenapoz()
+        //{
+
+        //}
+    }
+}
